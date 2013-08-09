b392eb9188f14b6a903984652efd1892ba77cfac
coderkun <coderkun@users.noreply.github.com>
2013-08-09 02:41:06 +0200

    current state as framework

---
+++ views/error.tpl
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+	<title>Service Unavailable</title>
+</head>
+
+<body>
+	<p>Die Anwendung steht zur Zeit leider nicht zur Verfügung.</p>
+</body>
+
+</html>
---
+++ www/index.php
@@ -0,0 +1,45 @@
+<?php
+
+	/**
+	 * NRE
+	 *
+	 * @author	coderkun <olli@coderkun.de>
+	 * @copyright	2013 coderkun (http://www.coderkun.de)
+	 * @license	http://www.gnu.org/licenses/gpl.html
+	 * @link	http://www.coderkun.de/projects/nre
+	 */
+	
+	/**
+	 * Define constants
+	 */
+	// Directory separator
+	if(!defined('DS')) {
+		define('DS', DIRECTORY_SEPARATOR);
+	}
+	// Root directory
+	if(!defined('ROOT')) {
+		define('ROOT', dirname(dirname(__FILE__)));
+	}
+	
+	
+	/**
+	 * De-/Activate error messages
+	 */
+	if($_SERVER['SERVER_ADDR'] == '127.0.0.1') {
+		error_reporting(E_ALL);
+		ini_set('display_errors', 1);
+		ini_set('log_errors', 0);
+	}
+	else {
+		error_reporting(E_ALL);
+		ini_set('display_errors', 0);
+		ini_set('log_errors', 1);
+	}
+
+	
+	/**
+	 * Run application
+	 */
+	require ROOT.DS.'bootstrap.inc';
+
+?>
