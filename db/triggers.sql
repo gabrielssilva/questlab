daf63951e0f987fea8f64a67b196eaebbd99567d
oliver <coderkun@users.noreply.github.com>
2015-05-19 12:24:24 +0200

    set definer for database triggers

--- db/triggers.sql
+++ db/triggers.sql
@@ -2,7 +2,7 @@
 
 DROP TRIGGER IF EXISTS update_cache_characters_insert;
 DELIMITER $$
-CREATE TRIGGER `update_cache_characters_insert`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_characters_insert`
 AFTER INSERT ON characters
 FOR EACH ROW BEGIN
 	CALL update_cache_characters(NEW.id);
@@ -11,7 +11,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_quests_update;
 DELIMITER $$
-CREATE TRIGGER `update_cache_quests_update`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_quests_update`
 AFTER UPDATE ON quests
 FOR EACH ROW BEGIN
 	CALL update_cache_characters_quest(NEW.id);
@@ -20,7 +20,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_quests_characters_insert;
 DELIMITER $$
-CREATE TRIGGER `update_cache_quests_characters_insert`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_quests_characters_insert`
 AFTER INSERT ON quests_characters
 FOR EACH ROW BEGIN
 	CALL update_cache_characters(NEW.character_id);
@@ -29,7 +29,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_quests_characters_update;
 DELIMITER $$
-CREATE TRIGGER `update_cache_quests_characters_update`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_quests_characters_update`
 AFTER UPDATE ON quests_characters
 FOR EACH ROW BEGIN
 	CALL update_cache_characters(OLD.character_id);
@@ -39,7 +39,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_quests_characters_delete;
 DELIMITER $$
-CREATE TRIGGER `update_cache_quests_characters_delete`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_quests_characters_delete`
 AFTER DELETE ON quests_characters
 FOR EACH ROW BEGIN
 	CALL update_cache_characters(OLD.character_id);
@@ -53,7 +53,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_charactergroups_insert;
 DELIMITER $$
-CREATE TRIGGER `update_cache_charactergroups_insert`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_charactergroups_insert`
 AFTER INSERT ON charactergroups
 FOR EACH ROW BEGIN
 	CALL update_cache_charactergroups(NEW.id);
@@ -62,7 +62,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_charactergroupsquests_update;
 DELIMITER $$
-CREATE TRIGGER `update_cache_charactergroupsquests_update`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_charactergroupsquests_update`
 AFTER UPDATE ON charactergroupsquests
 FOR EACH ROW BEGIN
 	CALL update_cache_charactergroups_charactergroupsquest(NEW.id);
@@ -71,7 +71,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_charactergroupsquests_groups_insert;
 DELIMITER $$
-CREATE TRIGGER `update_cache_charactergroupsquests_groups_insert`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_charactergroupsquests_groups_insert`
 AFTER INSERT ON charactergroupsquests_groups
 FOR EACH ROW BEGIN
 	CALL update_cache_charactergroups(NEW.charactergroup_id);
@@ -80,7 +80,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_charactergroupsquests_groups_update;
 DELIMITER $$
-CREATE TRIGGER `update_cache_charactergroupsquests_groups_update`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_charactergroupsquests_groups_update`
 AFTER UPDATE ON charactergroupsquests_groups
 FOR EACH ROW BEGIN
 	CALL update_cache_charactergroups(OLD.charactergroup_id);
@@ -90,7 +90,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_charactergroupsquests_groups_delete;
 DELIMITER $$
-CREATE TRIGGER `update_cache_charactergroupsquests_groups_delete`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_charactergroupsquests_groups_delete`
 AFTER DELETE ON charactergroupsquests_groups
 FOR EACH ROW BEGIN
 	CALL update_cache_charactergroups(OLD.charactergroup_id);
@@ -104,7 +104,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_characters_charactergroups_insert;
 DELIMITER $$
-CREATE TRIGGER `update_cache_characters_charactergroups_insert`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_characters_charactergroups_insert`
 AFTER INSERT ON characters_charactergroups
 FOR EACH ROW BEGIN
 	CALL update_cache_characters(NEW.character_id);
@@ -113,7 +113,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_characters_charactergroups_update;
 DELIMITER $$
-CREATE TRIGGER `update_cache_characters_charactergroups_update`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_characters_charactergroups_update`
 AFTER UPDATE ON characters_charactergroups
 FOR EACH ROW BEGIN
 	CALL update_cache_characters(OLD.character_id);
@@ -123,7 +123,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_characters_charactergroups_delete;
 DELIMITER $$
-CREATE TRIGGER `update_cache_characters_charactergroups_delete`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_characters_charactergroups_delete`
 AFTER DELETE ON characters_charactergroups
 FOR EACH ROW BEGIN
 	CALL update_cache_characters(OLD.character_id);
@@ -137,7 +137,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_cache_charactergroups_insert;
 DELIMITER $$
-CREATE TRIGGER `update_cache_cache_charactergroups_insert`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_cache_charactergroups_insert`
 AFTER INSERT ON cache_charactergroups
 FOR EACH ROW BEGIN
 	CALL update_cache_characters_charactergroup(NEW.charactergroup_id);
@@ -146,7 +146,7 @@ DELIMITER ;
 
 DROP TRIGGER IF EXISTS update_cache_cache_charactergroups_update;
 DELIMITER $$
-CREATE TRIGGER `update_cache_cache_charactergroups_update`
+CREATE DEFINER = 'z'@'%' TRIGGER `update_cache_cache_charactergroups_update`
 AFTER UPDATE ON cache_charactergroups
 FOR EACH ROW BEGIN
 	CALL update_cache_characters_charactergroup(NEW.charactergroup_id);
